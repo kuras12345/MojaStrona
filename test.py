c0a74958f7e9211f21b508348e5dfa923ec6277e
kuras12345 <74146850+kuras12345@users.noreply.github.com>
2026-04-06 15:15:55 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from kivy.uix.label import Label
 class TestApp(App):
     def build(self):
 
-         return Label(text='Hello World!!')
+         return Label(text='Hello World!')
 
 if __name__ == '__main__':
  TestApp().run()
